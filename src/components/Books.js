6700f3827101c70e303398110a8d21e152136dcd
MickeyRider01 <s5935512082@phuket.psu.ac.th>
2020-04-24 23:39:53 +0700

    fix

--- src/components/Books.js
+++ src/components/Books.js
@@ -7,10 +7,12 @@ export default () => {
         <div className='LoginPage-Container'>
             <h1>Welcome To Book Store</h1>
             <div className='SigninContainer'>
-                <tbody>
-                    <input className='input' type="text"/><br/>
-                    <input className='input' type="text"/>
-                </tbody>
+                <input className='input' type="text"/><br/>
+                <input className='input' type="text"/>
+            </div>
+            <div className='btnContainer'>
+                <button className='btn' /*onClick={()=>....}*/>Sign in</button>
+                <button className='btn' /*onClick={()=>....}*/>Sign up</button>
             </div>
         </div>
     )
